272fda7d69277d3b7876ab4144b6796e65451c7e
Sumant Shah <sumant.shah@gigacars.com>
2024-05-22 12:11:29 +0530

    slideshow logic and design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from 'react';
+import './style.css';
+
+const arr = [
+  "./image/image-1.jpg",
+  "./image/image-2.jpg",
+  "./image/image-3.jpg",
+  "./image/image-5.jpg"
+]
 
 function App() {
+  const [imageList, setImageList] = useState([]);
+  const [addClassName, setaddClassName] = useState('');
+
+  useEffect(() => {
+    setImageList(arr)
+  }, [])
+
+
+  const moveSlider = (direction) => {
+    if (direction == 'next') {
+      let nextList = imageList;
+      let pop = nextList.shift();
+      nextList.push(pop);
+      setaddClassName('next')
+      setImageList([...nextList]);
+    } else {
+      let prevList = imageList;
+      let pop = prevList.pop();
+      prevList.unshift(pop);
+      setaddClassName('prev')
+      setImageList([...prevList]);
+    }
+  }
+
+  const animationEnd = () => {
+    setaddClassName('')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="right">
+        <div className={`slider ${addClassName}`} onAnimationEnd={() => animationEnd()}>
+          <div className="list">
+            {
+              imageList.map((item) => {
+                return (
+                  <div className="item">
+                    <img src={item} alt="image 1" />
+                  </div>
+                )
+              })
+            }
+          </div>
+
+          <div className="nextPrevArrows">
+            <button onClick={() => moveSlider('prev')} className="prev"> {`<`}</button>
+            <button onClick={() => moveSlider('next')} className="next"> {`>`} </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
